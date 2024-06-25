c7ad3765143b31bc61e252758487826fb57a75a0
lazzaric92 <lazzaric_92@hotmail.it>
2024-06-25 18:53:15 +0200

    added $newIncipit with str_replace

--- index.php
+++ index.php
@@ -1,5 +1,7 @@
 <?php 
-    $incipit = 'Quando il signor Bilbo Baggins di Casa Baggins annunziò che avrebbe presto festeggiato il suo centoundicesimo compleanno con una festa oltremodo fastosa, i commenti e i fermenti a Hobbiton si sprecarono. Bilbo era ricchissimo e alquanto stravagante e, fin dalla straordinaria sparizione, seguita dal ritorno inaspettato, era stato il prodigio della Contea. Le ricchezze riportate dai viaggi erano ormai diventate una leggenda locale e secondo la credenza popolare, inutilmente smentita dagli anziani, la Collina a Casa Baggins era piena di gallerie imbottite di tesori. E se questo non bastava a dargli fama, a stupire era il vigore inesausto. Il tempo passava, ma il signor Baggins non sembrava risentirne più di tanto. A novant’anni era più o meno come a cinquanta. A novantanove iniziarono a definirlo ben conservato: con immutato ci sarebbero andati più vicini. C’era chi scuoteva la testa convinto che il troppo stroppia; non sembrava giusto che qualcuno possedesse una giovinezza (manifestamente) perpetua e al tempo stesso una ricchezza (verosimilmente) inesauribile. “Toccherà scontarla,” dicevano. “Non è naturale e sarà fonte di guai”.'
+    $incipit = 'Quando il signor Bilbo Baggins di Casa Baggins annunziò che avrebbe presto festeggiato il suo centoundicesimo compleanno con una festa oltremodo fastosa, i commenti e i fermenti a Hobbiton si sprecarono. Bilbo era ricchissimo e alquanto stravagante e, fin dalla straordinaria sparizione, seguita dal ritorno inaspettato, era stato il prodigio della Contea. Le ricchezze riportate dai viaggi erano ormai diventate una leggenda locale e secondo la credenza popolare, inutilmente smentita dagli anziani, la Collina a Casa Baggins era piena di gallerie imbottite di tesori. E se questo non bastava a dargli fama, a stupire era il vigore inesausto. Il tempo passava, ma il signor Baggins non sembrava risentirne più di tanto. A novant’anni era più o meno come a cinquanta. A novantanove iniziarono a definirlo ben conservato: con immutato ci sarebbero andati più vicini. C’era chi scuoteva la testa convinto che il troppo stroppia; non sembrava giusto che qualcuno possedesse una giovinezza (manifestamente) perpetua e al tempo stesso una ricchezza (verosimilmente) inesauribile. “Toccherà scontarla,” dicevano. “Non è naturale e sarà fonte di guai”.';
+    $badword = $_GET["badword"];
+    $newIncipit = str_replace($badword, "***", $incipit);
 ?>
 
 <!DOCTYPE html>
@@ -16,11 +18,16 @@
         <p>Word count: <?php echo strlen($incipit) ?> </p>
         
         <br>
-        <form action="index.php" method="GET">
+        <form action="./index.php" method="GET">
             <label for="badword">Choose a word:</label>
             <input type="text" name="badword" id="badword">
             <button type="submit">Ban that word!</button>
         </form>
+
+        <br>
+        <h2>Censored incipit</h2>
+        <p> <?php echo $newIncipit ?> </p>
+        <p>Word count: <?php echo strlen($newIncipit) ?> </p>
     </main>
 </body>
 </html>
