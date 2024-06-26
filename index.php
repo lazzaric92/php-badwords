6df7245578bb556e4c3748ac9b2f0b5b9e670086
lazzaric92 <lazzaric_92@hotmail.it>
2024-06-26 10:06:13 +0200

    added ternary

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
     $incipit = 'Quando il signor Bilbo Baggins di Casa Baggins annunziò che avrebbe presto festeggiato il suo centoundicesimo compleanno con una festa oltremodo fastosa, i commenti e i fermenti a Hobbiton si sprecarono. Bilbo era ricchissimo e alquanto stravagante e, fin dalla straordinaria sparizione, seguita dal ritorno inaspettato, era stato il prodigio della Contea. Le ricchezze riportate dai viaggi erano ormai diventate una leggenda locale e secondo la credenza popolare, inutilmente smentita dagli anziani, la Collina a Casa Baggins era piena di gallerie imbottite di tesori. E se questo non bastava a dargli fama, a stupire era il vigore inesausto. Il tempo passava, ma il signor Baggins non sembrava risentirne più di tanto. A novant’anni era più o meno come a cinquanta. A novantanove iniziarono a definirlo ben conservato: con immutato ci sarebbero andati più vicini. C’era chi scuoteva la testa convinto che il troppo stroppia; non sembrava giusto che qualcuno possedesse una giovinezza (manifestamente) perpetua e al tempo stesso una ricchezza (verosimilmente) inesauribile. “Toccherà scontarla,” dicevano. “Non è naturale e sarà fonte di guai”.';
     $badword = $_GET["badword"];
     $newIncipit = str_replace($badword, "***", $incipit);
+    $_GET['badword'] = isset($_GET['badword']) ? $_GET['badword'] : '';
 ?>
 
 <!DOCTYPE html>
@@ -15,7 +16,7 @@
     <main>
         <h1>Il Signore degli Anelli</h1>
         <p> <?php echo $incipit ?> </p>
-        <p>Word count: <?php echo strlen($incipit) ?> </p>
+        <p>Characters count: <?php echo strlen($incipit) ?> </p>
         
         <br>
         <form action="./index.php" method="GET">
@@ -27,7 +28,7 @@
         <br>
         <h2>Censored incipit</h2>
         <p> <?php echo $newIncipit ?> </p>
-        <p>Word count: <?php echo strlen($newIncipit) ?> </p>
+        <p>Characters count: <?php echo strlen($newIncipit) ?> </p>
     </main>
 </body>
 </html>
